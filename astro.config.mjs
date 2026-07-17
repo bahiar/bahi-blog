2d5c7e0dfd662d2b13c648ec4eeeec15436c47eb
bahiar <bahiarsalud@gmail.com>
2026-07-17 12:34:14 -0300

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  // Tu URL pública base de GitHub Pages
-  site: 'https://bahiar.github.io',
-  // El nombre exacto de tu repositorio con barras diagonales
-  base: '/bahi-blog',
-});
